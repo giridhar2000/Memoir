b5c70fff63804316976ef898f10f1951398723f3
GiridharHUB <scintillantmail@gmail.com>
2023-12-27 11:41:16 +0530

    changes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
+require('dotenv').config();
 
 const app = express();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT
 
 app.use(express.json());
 app.use(cors());
